7bf981c31e3844946b8c66b6fabcba94df6283a7
thanhtoan1742 <thanhtoan1742@gmail.com>
2020-12-01 11:59:40 +0700

    finished d_CO2_Air and d_CO2_Top

--- Code/Main/CO2_Model.py
+++ Code/Main/CO2_Model.py
@@ -5,9 +5,30 @@ class CO2_Model:
         self.t = 0
         
     def d_CO2_Air(self):
-        return 0
+        return self.MC_BlowAir() + self.MC_ExtAir() + self.MC_PadAir() - self.MC_AirCan() - self.MC_AirTop() - self.MC_AirOut()
 
     def d_CO2_Top(self):
+        return self.MC_AirTop() - self.MC_TopOut()
+
+    def MC_BlowAir(self):
+        return 0
+
+    def MC_ExtAir(self):
+        return 0
+
+    def MC_PadAir(self):
+        return 0
+
+    def MC_AirCan(self):
+        return 0
+
+    def MC_AirTop(self):
+        return 0
+
+    def MC_AirOut(self):
+        return 0
+
+    def MC_TopOut(self):
         return 0
     
     def evaluate(self, CO2_Air_0, CO2_Top_0, t):
