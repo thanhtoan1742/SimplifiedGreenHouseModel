86a8042f2cb4d9ae54bb8d642015fc8b49c29241
duytran-vio <ducduyvio@gmail.com>
2020-12-23 14:12:47 +0700

    add log 20201223

--- Code/Main/ModelState.py
+++ Code/Main/ModelState.py
@@ -1,8 +1,6 @@
 class ModelState:
     def __init__(self, t, T_Air, T_Top, CO2_Air, CO2_Top, VP_Air, VP_Top):
         self.t = t
-        self.T_Air = T_Air
-        self.T_Top = T_Top
         self.CO2_Air = CO2_Air
         self.C02_Top = CO2_Top
         self.VP_Air = VP_Air
