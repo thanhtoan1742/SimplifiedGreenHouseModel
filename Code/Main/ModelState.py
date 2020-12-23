7177469127a4b6cd44df061da43e01f8c473628f
Nguyen <voxuannguyen2001@gmail.com>
2020-12-23 15:20:56 +0700

    Add update() in model state ver 1.0

--- Code/Main/ModelState.py
+++ Code/Main/ModelState.py
@@ -1,7 +1,19 @@
 class ModelState:
-    def __init__(self, t, T_Air, T_Top, CO2_Air, CO2_Top, VP_Air, VP_Top):
+    def __init__(self, t, CO2_Air, CO2_Top, VP_Air, VP_Top):
         self.t = t
         self.CO2_Air = CO2_Air
         self.CO2_Top = CO2_Top
         self.VP_Air = VP_Air
         self.VP_Top = VP_Top
+
+    def update(self, t=None, CO2_Air=None, CO2_Top=None, VP_Air=None, VP_Top=None):
+        if t is not None:
+            self.t = t
+        if CO2_Air is not None:
+            self.CO2_Air = CO2_Air
+        if CO2_Top is not None:
+            self.CO2_Top = CO2_Top
+        if VP_Air is not None:
+            self.VP_Air = VP_Air
+        if VP_Top is not None:
+            self.VP_Top = VP_Top
