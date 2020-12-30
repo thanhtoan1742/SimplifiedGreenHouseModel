bc4e88f00c390a8705d911faf04b9f30c1c4f0ef
thanhtoan1742 <thanhtoan1742@gmail.com>
2020-12-30 10:18:26 +0700

    changed

--- Code/Main/ModelParameter.py
+++ Code/Main/ModelParameter.py
@@ -17,7 +17,7 @@ class ModelParameter:
         self.A_Flr = A_Flr # 1.3e4 | 1.4e4 | 7.8e4 | 278
         self.A_Roof = A_Roof # 0.2 * 1.3e4 | 0.1 * 1.4e4 | 0.18 * 7.8e4 | x
         self.A_Side = A_Side # 0 | 0 | 0 | x
-        self.A_Cov = A_Cov
+        self.A_Cov = A_Cov # 1.7e4 | 1.8e4 | 9.0e4 | 730
 
         self.C_Gh_d = C_Gh_d # 0.75 | 0.75 | 0.65 | x
         self.C_Gh_w = C_Gh_w # 0.12 | 0.12 | 0.09 | x
@@ -34,11 +34,6 @@ class ModelParameter:
 
         self.phi_VentForced = 0 # x | x | x | 0
 
-        self.P_MLT = P_MLT
-        self.L_0 = L_0
-        self.K = K
-        self.k_T_opt = k_T_opt
-        self.S = S
         self.LAI = LAI # Vanthoor page 31
         self.c_HECin = c_HECin # 2.21 | 1.86 | 1.86 | 2.21
         self.COP_MechCool = COP_MechCool # x | x | x | x 
